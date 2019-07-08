d20933cbcf83cc82dea12560e24de22ce457c24a
Ramon Gomez <rgomez17@cmc.edu>
2019-07-08 02:24:25 -0700

    updates

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import today_preview
-import predictions
 
+# import pickle
+# import final_preview
 
 app = Flask(__name__)
-
-
-# Save references to each table
-# Samples_Metadata = Base.classes.sample_metadata
-# Samples = Base.classes.samples
 
 #################################################
 # Flask Routes
@@ -20,40 +15,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """Return the homepage."""
-    # print (today_preview)
-
-    today_preview.split_record
-	df = today_preview.previewext
-#     return render_template("index.html")
-# 
-#     return jsonify(list(df.columns)[2:])
 
     return render_template("index.html")
 
 @app.route("/outcomes")
 def outcomes():
-    """Return a list of team names."""
+    """Return a list of predictions and team names."""
 
     return render_template("outcomes.html")
-    # console.log("boss")
+    
+@app.route("/subscribe")
+def subscribe():
+    """Return form to subscribe."""
+    return render_template("subscribe.html")
+
+
+@app.route("/accuracy")
+def accuracy():
+    """Return model accuracy."""
+    return render_template("accuracy.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-# Save references to each table
-# Samples_Metadata = Base.classes.sample_metadata
-# Samples = Base.classes.samples
-
-
-
-
-# @app.route("/outcomes")
-# def names():
-#     """Return a list of sample names."""
-
-    # # Use Pandas to perform the sql query
-    # stmt = db.session.query(Samples).statement
-    # df = pd.read_sql_query(stmt, db.session.bind)
-
-    # # Return a list of the column names (sample names)
-    # return jsonify(list(df.columns)[2:])
+    app.run(host= '0.0.0.0')
+    
